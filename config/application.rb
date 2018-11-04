09bd7d0cd4de7b9a2dd591a6d835296e3ca64998
HirotoShibutani <creation.199484@gmail.com>
2018-11-04 22:06:25 +0900

    [update] エラーメッセージを日本語で表示する

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Hirrot
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     # config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
