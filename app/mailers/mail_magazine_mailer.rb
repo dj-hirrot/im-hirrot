cdcdf8a70c715d8fee726aee8cde4708cbdc185d
HirotoShibutani <creation.199484@gmail.com>
2018-11-06 23:51:49 +0900

    [update] メールタイトルの修正

--- app/mailers/mail_magazine_mailer.rb
+++ app/mailers/mail_magazine_mailer.rb
@@ -1,6 +1,6 @@
 class MailMagazineMailer < ApplicationMailer
   def alert_develop_comment
     @user = params[:user]
-    mail(to: @user.email, subject: '新しいユーザー登録のお知らせ')
+    mail(to: @user.email, subject: 'コメントができるようになりました')
   end
 end
