b67c5d94342927f13b3011a9e48aa362b90dc412
HirotoShibutani <creation.199484@gmail.com>
2018-11-08 06:58:10 +0900

    [update] ログイン促進

---
+++ app/assets/javascripts/blogs.js
@@ -0,0 +1,9 @@
+function promptLogin() {
+  swal({
+    title: "ログインしてください！",
+    text: "お気に入りはログインすることで利用できます。お気に入りすることで、気になる記事を保存することができるようになります。",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  });
+}
